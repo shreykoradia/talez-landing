e2a0eeb6c9df389a2e34d171adac6d3d1b24fc60
Shrey Koradia <shrey.radicalloop@gmail.com>
2024-10-19 10:58:52 +0530

    fixed talez

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center p-4 w-full">
+    <div className="flex justify-between items-center p-4 w-full max-xl:!bg-black">
       <div>
         <h2 className="text-2xl font-semibold text-primary">Talez</h2>
         <p className="text-muted text-xs font-medium">a brain for your apps</p>
